d4a3c8c457eca8252f0476e519621373a533882e
furkancanbicim <furkancanbicim@gmail.com>
2022-06-08 18:42:47 +0300

    first commit

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class MyDictionary<TKey, TValue>
+    {
+        TKey[] _array;
+        TKey[] _tempArray;
+        TValue[] _array2;
+        TValue[] _tempArray2;
+        public MyDictionary()
+        {
+            _array = new TKey[0];
+            _array2 = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+
+            _tempArray = _array;
+            _tempArray2 = _array2;
+            _array = new TKey[_tempArray.Length + 1];
+            _array2 = new TValue[_tempArray2.Length + 1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+                _array2[i] = _tempArray2[i];
+            }
+            _array[_array.Length - 1] = key;
+            _array2[_array2.Length - 1] = value;
+        }
+
+        public object? this[TKey key]
+        {
+            get
+            {
+                for (int i = 0; i < _array.Length; i++)
+                {
+                    if (Equals(_array[i], key))
+                    {
+                        return _array2[i];
+                    }
+                }
+                return default(TValue);
+            }
+        }
+
+        public int Count
+        {
+            get { return _array.Length; }
+        }
+    }
+
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using MyDictionary;
+
+
+
+MyDictionary<int, string> myDictonary = new MyDictionary<int, string>();
+myDictonary.Add(0, "Kerem ÖZER");
+myDictonary.Add(1, "Ali RODOPLU");
+myDictonary.Add(2, "Okan BİLİR");
+myDictonary.Add(3, "Sinan ERTAN");
+myDictonary.Add(4, "Fukan BİÇER");
+myDictonary.Add(5, "Emirhan DAĞLIOĞLU");
+
+Console.WriteLine("Dizinin genişliği = "+myDictonary.Count);
+
+Console.WriteLine(myDictonary[3]);
+
+MyDictionary<string, string> myDictonary1 = new MyDictionary<string, string>();
+myDictonary1.Add("Baba", "Kerem ÖZER");
+myDictonary1.Add("Dayı", "Ali RODOPLU");
+myDictonary1.Add("Amca", "Okan BİLİR");
+
+Console.WriteLine("Dizinin genişliği = " + myDictonary1.Count);
+
+Console.WriteLine(myDictonary1["Dayı"]);
+
+
+
+
